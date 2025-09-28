7fcc2681f6c12ee09a8696b5b906a5e555636997
farzad-cell <farzaddarwazi869@gmail.com>
2025-09-27 23:21:56 -0700

    Add files via upload

---
+++ Calculator/calculator.cpp
@@ -0,0 +1,65 @@
+//
+//Created by Farzad Darwazi on 9/6/2025.
+//
+
+#include <iostream>
+#include <cmath>
+
+
+
+using namespace std;
+
+double num1, num2, result;
+char op;
+
+
+
+
+int main()
+{
+
+    cout << "Welcome to Farzad Darwazi's calculator.\tUse ONLY one operator at a time (+, -, *, /, ^, or 5550$ for sqrt).\n Enter (0+0) to exit the calculator" << endl;
+
+
+
+    while (true) {
+        if (!(cin >> num1 >> op >> num2)) {
+            cin.clear();
+            cin.ignore(99999, '\n');
+            cout << "Invalid operation. Please try again ONLY use numbers and operators. EX, (1+2)" << endl;
+            continue;
+        }
+
+        if (op == '+' ) {
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+        }else if (op == '-' ) {
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+        }else if (op == '*' ) {
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+        }else if (op == '$' ) {
+            result = sqrt(num2);
+            cout << "Result: " << result << endl;
+        }else if (op == '^'){
+            result = pow(num1, num2);
+            cout << "Result: " << result << endl;
+        }else if (op == '/' ) {
+
+            if (num2 == 0) {
+                cout << " :D LOL You can NOT divide by zero in math??" << endl;
+                continue;
+
+            }
+            result = num1 / num2;
+            cout << "Result: " << result << endl;
+        }
+
+        if (num1 == 0 && num2 == 0) {
+            cout << "Thanks for using Farzad's calculator, Bye!" << endl;
+            return 0;
+        }
+    }
+
+}
